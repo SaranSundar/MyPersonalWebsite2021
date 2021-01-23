7a1a2cc8c2fe360aa744b775abf1b73e49c02eab
SaranSundar <saransszg@gmail.com>
2021-01-22 18:21:44 -0600

    Added blog posts

---
+++ components/BlogPost.js
@@ -0,0 +1,49 @@
+export default function BlogPost({
+                                     blogType = "Article",
+                                     blogTitle = "Boost your conversion rate",
+                                     blogDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto\n" +
+                                     "                            accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos\n" +
+                                     "                            quis dolorum.",
+                                     blogThumbnailSrc = "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+                                     blogPostingDate = "Mar 16, 2020",
+                                     blogReadInMinDuration = "6"
+                                 }) {
+    return (
+
+        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <div className="flex-shrink-0">
+                <img className="h-48 w-full object-cover"
+                     src={blogThumbnailSrc}
+                     alt=""/>
+            </div>
+            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                    <p className="text-sm font-medium text-indigo-600">
+                        <a href="/" className="hover:underline">
+                            {blogType}
+                        </a>
+                    </p>
+                    <a href="/" className="block mt-2">
+                        <p className="text-xl font-semibold text-gray-900">
+                            {blogTitle}
+                        </p>
+                        <p className="mt-3 text-base text-gray-500">
+                            {blogDescription}
+                        </p>
+                    </a>
+                </div>
+                <div className="mt-6 flex items-center">
+                    <div className="flex space-x-1 text-sm text-gray-500">
+                        {blogPostingDate}&nbsp;
+                        <span aria-hidden="true">
+                              &#xB7;
+                            </span>
+                        <span>
+                            {blogReadInMinDuration} min read
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/explore-portfolio.js
+++ pages/explore-portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProgressiveImageLoader from "components/ProgressiveImageLoader";
+import BlogPost from "components/BlogPost";
 
 const PortfolioPage = () => {
     return (
@@ -29,60 +30,7 @@ const PortfolioPage = () => {
                 </div>
                 <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
 
-                    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                        <div className="flex-shrink-0">
-                            <img className="h-48 w-full object-cover"
-                                 src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-                                 alt=""/>
-                        </div>
-                        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                            <div className="flex-1">
-                                <p className="text-sm font-medium text-indigo-600">
-                                    <a href="/" className="hover:underline">
-                                        Article
-                                    </a>
-                                </p>
-                                <a href="/" className="block mt-2">
-                                    <p className="text-xl font-semibold text-gray-900">
-                                        Boost your conversion rate
-                                    </p>
-                                    <p className="mt-3 text-base text-gray-500">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-                                        accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos
-                                        quis dolorum.
-                                    </p>
-                                </a>
-                            </div>
-                            <div className="mt-6 flex items-center">
-                                <div className="flex-shrink-0">
-                                    <a href="/">
-                                        <span className="sr-only">Roel Aufderehar</span>
-                                        <img className="h-10 w-10 rounded-full"
-                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                             alt=""/>
-                                    </a>
-                                </div>
-                                <div className="ml-3">
-                                    <p className="text-sm font-medium text-gray-900">
-                                        <a href="/" className="hover:underline">
-                                            Roel Aufderehar
-                                        </a>
-                                    </p>
-                                    <div className="flex space-x-1 text-sm text-gray-500">
-                                        <time dateTime="2020-03-16">
-                                            Mar 16, 2020
-                                        </time>
-                                        <span aria-hidden="true">
-                      &#xB7;
-                    </span>
-                                        <span>
-                      6 min read
-                    </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <BlogPost/>
 
                     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
                         <div className="flex-shrink-0">
@@ -128,11 +76,11 @@ const PortfolioPage = () => {
                                             Mar 10, 2020
                                         </time>
                                         <span aria-hidden="true">
-                      &#xB7;
-                    </span>
+                                          &#xB7;
+                                        </span>
                                         <span>
-                      4 min read
-                    </span>
+                                          4 min read
+                                        </span>
                                     </div>
                                 </div>
                             </div>
@@ -183,11 +131,11 @@ const PortfolioPage = () => {
                                             Feb 12, 2020
                                         </time>
                                         <span aria-hidden="true">
-                      &#xB7;
-                    </span>
+                                          &#xB7;
+                                        </span>
                                         <span>
-                      11 min read
-                    </span>
+                                          11 min read
+                                        </span>
                                     </div>
                                 </div>
                             </div>
