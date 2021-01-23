1bce0f4f6a16c9f9c6a39393fce1ef0d55ebb68c
SaranSundar <saransszg@gmail.com>
2021-01-23 02:41:27 -0600

    mobile navbar complete

--- components/MobileNavBar.js
+++ components/MobileNavBar.js
@@ -1,27 +1,37 @@
 import {Menu, Transition} from "@headlessui/react";
+import GithubSVG from "public/images/navbar/github.svg"
+import YoutubeSVG from "public/images/navbar/youtube.svg"
+import LinkedinSVG from "public/images/navbar/linkedin.svg"
+import {openInNewTab} from "../utils/Utils";
 
 export default function MobileNavBar() {
     return (
-        <div className="flex items-center justify-center p-12 z-50">
+        <div className="-mr-2 flex items-center justify-center z-50 sm:hidden">
             <div className="relative inline-block text-left">
                 <Menu>
                     {({open}) => (
                         <>
-                              <span className="rounded-md shadow-sm">
+                            {console.log("OPEN IS " + open)}
+                            <span className="rounded-md shadow-sm">
                                 <Menu.Button
-                                    className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
-                                  <span>Options</span>
-                                  <svg
-                                      className="w-5 h-5 ml-2 -mr-1"
-                                      viewBox="0 0 20 20"
-                                      fill="currentColor"
-                                  >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clipRule="evenodd"
-                                    />
-                                  </svg>
+                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                  <span className="sr-only">Open main menu</span>
+                                    {/* Icon when menu is closed. */}
+                                    {!open &&
+                                    <svg className="block h-6 w-6"
+                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke="currentColor" aria-hidden="true">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                              d="M4 6h16M4 12h16M4 18h16"/>
+                                    </svg>}
+                                    {/* Icon when menu is open. */}
+                                    {open &&
+                                    <svg className="block h-6 w-6"
+                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke="currentColor" aria-hidden="true">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                              d="M6 18L18 6M6 6l12 12"/>
+                                    </svg>}
                                 </Menu.Button>
                               </span>
 
@@ -38,77 +48,86 @@ export default function MobileNavBar() {
                                     static
                                     className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
                                 >
-                                    <div className="px-4 py-3">
-                                        <p className="text-sm leading-5">Signed in as</p>
-                                        <p className="text-sm font-medium leading-5 text-gray-900 truncate">
-                                            tom@example.com
-                                        </p>
-                                    </div>
 
                                     <div className="py-1">
                                         <Menu.Item>
                                             {({active}) => (
                                                 <a
-                                                    href="#account-settings"
                                                     className={`${
                                                         active
                                                             ? "bg-gray-100 text-gray-900"
                                                             : "text-gray-700"
-                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left cursor-pointer`}
                                                 >
-                                                    Account settings
+                                                    Explore Portfolio
                                                 </a>
                                             )}
                                         </Menu.Item>
                                         <Menu.Item>
                                             {({active}) => (
                                                 <a
-                                                    href="#support"
                                                     className={`${
                                                         active
                                                             ? "bg-gray-100 text-gray-900"
                                                             : "text-gray-700"
-                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left cursor-pointer`}
                                                 >
-                                                    Support
+                                                    View Resume
                                                 </a>
                                             )}
-                                        </Menu.Item>
-                                        <Menu.Item
-                                            as="span"
-                                            disabled
-                                            className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 cursor-not-allowed opacity-50"
-                                        >
-                                            New feature (soon)
                                         </Menu.Item>
                                         <Menu.Item>
                                             {({active}) => (
                                                 <a
-                                                    href="#license"
                                                     className={`${
                                                         active
                                                             ? "bg-gray-100 text-gray-900"
                                                             : "text-gray-700"
-                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left cursor-pointer`}
                                                 >
-                                                    License
+                                                    About Me
                                                 </a>
                                             )}
                                         </Menu.Item>
                                     </div>
 
-                                    <div className="py-1">
+                                    <div className="py-1 flex">
                                         <Menu.Item>
                                             {({active}) => (
-                                                <a
-                                                    href="#sign-out"
+                                                <a onClick={() => openInNewTab("https://www.youtube.com/channel/UCI1-IN8JwmFxtY_eVcIATTg")}
                                                     className={`${
                                                         active
                                                             ? "bg-gray-100 text-gray-900"
                                                             : "text-gray-700"
-                                                    } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                                                    } flex justify-between w-full px-4 py-1 text-sm leading-5 text-left cursor-pointer`}
                                                 >
-                                                    Sign out
+                                                    <YoutubeSVG height={40} width={40}/>
+                                                </a>
+                                            )}
+                                        </Menu.Item>
+                                        <Menu.Item>
+                                            {({active}) => (
+                                                <a onClick={() => openInNewTab("https://github.com/SaranSundar")}
+                                                    className={`${
+                                                        active
+                                                            ? "bg-gray-100 text-gray-900"
+                                                            : "text-gray-700"
+                                                    } flex justify-between w-full px-4 py-1 text-sm leading-5 text-left cursor-pointer`}
+                                                >
+                                                    <GithubSVG height={35} width={35}/>
+                                                </a>
+                                            )}
+                                        </Menu.Item>
+                                        <Menu.Item>
+                                            {({active}) => (
+                                                <a onClick={() => openInNewTab("https://www.linkedin.com/in/saran-sundararajan/")}
+                                                    className={`${
+                                                        active
+                                                            ? "bg-gray-100 text-gray-900"
+                                                            : "text-gray-700"
+                                                    } flex justify-between w-full px-4 py-1 text-sm leading-5 text-left cursor-pointer`}
+                                                >
+                                                    <LinkedinSVG height={35} width={35}/>
                                                 </a>
                                             )}
                                         </Menu.Item>
--- components/NavBar.js
+++ components/NavBar.js
@@ -4,13 +4,9 @@ import LinkedinSVG from "public/images/navbar/linkedin.svg"
 import Link from 'next/link'
 import {PAGE_ROUTES} from "utils/ROUTES";
 import MobileNavBar from "./MobileNavBar";
+import {openInNewTab} from "utils/Utils";
 
 const NavBar = () => {
-
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-    }
 
     return (
         <div className="bg-gray-100">
@@ -70,27 +66,6 @@ const NavBar = () => {
                         </div>
 
                         {/* Mobile menu button */}
-                        <div className="-mr-2 flex items-center sm:hidden">
-                            <button data-todo-at-click="open = !open"
-                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                    data-todo-x-bind-aria-expanded="open" aria-expanded="false">
-                                <span className="sr-only">Open main menu</span>
-                                {/* Icon when menu is closed. */}
-                                <svg className="block h-6 w-6"
-                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M4 6h16M4 12h16M4 18h16"/>
-                                </svg>
-                                {/* Icon when menu is open. */}
-                                <svg className="hidden h-6 w-6"
-                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
-                        </div>
                         <MobileNavBar/>
 
 
