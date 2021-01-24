2d9bc0d5e9de9ed10db24ec3b7da168d5bb57bb7
SaranSundar <saransszg@gmail.com>
2021-01-23 22:09:34 -0600

    can download resume now

--- pages/view-resume.js
+++ pages/view-resume.js
@@ -15,7 +15,7 @@ export default function ResumePage() {
             <div className="relative max-w-7xl mx-auto ">
                 <div className="flex">
                     <a type="button" href="/images/resume/resume.pdf" download="SaranSundar_Resume.pdf"
-                            className="mx-auto inline-flex items-center px-6 py-3 mt-10 -mb-5 z-50 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            className="mx-auto inline-flex items-center px-6 py-3 mt-10 mb-1 md:-mb-5 z-50 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <svg className="-ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                              fill="currentColor" aria-hidden="true">
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
